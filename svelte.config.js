74147de165391c6c8eddc21d3d1e6f738288617e
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-15 00:35:32 +0100

    production

--- svelte.config.js
+++ svelte.config.js
@@ -33,7 +33,7 @@ const config = {
 			strict: true,
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+            base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''
 		}
 	},
 	extensions: ['.svelte', '.svx', '.md']
