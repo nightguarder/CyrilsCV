ec20d99b036e05be7f8c9e148e16570d45580982
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-15 00:57:11 +0100

    hardcore it?

--- svelte.config.js
+++ svelte.config.js
@@ -33,7 +33,7 @@ const config = {
 			strict: true,
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''		}
+			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : '/CyrilsCV'		}
 		},
 	extensions: ['.svelte', '.svx', '.md']
 };
