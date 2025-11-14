830d344bdf57074a779c1ecf51e71c8beeb36ec2
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-15 00:31:11 +0100

    remove /

--- src/routes/+layout.js
+++
@@ -1,9 +0,0 @@
-// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
-// pages by setting this here, but should prerender as many as possible to
-// avoid large performance and SEO impacts
-// We're using adapter static so try to prerender all pages
-// https://svelte.dev/docs/kit/adapter-static
-export const prerender = true;
-
-// Handle base path for static adapter
-export const trailingSlash = 'always';
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,11 @@
 <script>
     import AboutContent from '$lib/content/about.svx';
-    import { base } from '$app/paths';
 </script>
 
 <nav class="mb-8">
-    <a href={base} class="mr-4 hover:underline">Home</a>
-    <a href={base + '/about'} class="hover:underline">About</a>
-    <a href={base + '/frontmatter'} class="hover:underline">Frontmatter</a>
+    <a href="/" class="mr-4 hover:underline">Home</a>
+    <a href="/about" class="hover:underline">About</a>
+    <a href="/frontmatter" class="hover:underline">Frontmatter</a>
 </nav>
 
 <main class="prose max-w-none">
