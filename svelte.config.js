970aa981e6311b50274ca72c0568cd78bcd787c8
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-15 00:50:55 +0100

    svelte config ok?

--- svelte.config.js
+++ svelte.config.js
@@ -33,9 +33,8 @@ const config = {
 			strict: true,
 		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''
-		}
-	},
+			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''		}
+		},
 	extensions: ['.svelte', '.svx', '.md']
 };
 
