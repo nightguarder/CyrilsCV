0dbf1970b012b787ed1b7c3ab2d40bbf90137ab6
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-14 22:23:00 +0100

    add svelte kit adapter for SSG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	export const prerender = true; // avoid large performance and SEO impacts
 	import favicon from '$lib/assets/favicon.svg';
-
+	
 	let { children } = $props();
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,18 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// https://svelte.dev/docs/kit/adapter-static
+		// Using adapter-static (SSG) to deploy to GitHub Pages
+		adapter: adapter(),
+		pages: 'build',
+		assets: 'build',
+		fallback: '404.html',
+		precompress: 'false',
+		strict: 'true',
+		trailingSlash: 'always' //Github Pages compatible
+	},
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	},
 	extensions: ['.svelte', '.svx']
 };
