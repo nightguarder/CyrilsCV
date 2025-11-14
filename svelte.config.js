8e519a6dcc1f74d935f4928ffeae8d36b7f27120
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-11-14 22:33:04 +0100

    clean d github workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,16 +10,14 @@ const config = {
 	kit: {
 		// https://svelte.dev/docs/kit/adapter-static
 		// Using adapter-static (SSG) to deploy to GitHub Pages
-		adapter: adapter(),
-		pages: 'build',
-		assets: 'build',
-		fallback: '404.html',
-		precompress: 'false',
-		strict: 'true',
-		trailingSlash: 'always' //Github Pages compatible
-	},
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		trailingSlash: 'always' // GitHub Pages compatible
 	},
 	extensions: ['.svelte', '.svx']
 };
